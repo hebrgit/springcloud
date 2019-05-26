e4679965e40e4776d6085deae6be5e47f1d5aea6
hebo_ext <bo_he@asdc.com.cn>
2019-05-26 15:59:15 +0800

    添加hystrix 仪表盘依赖

--- springcloud-api/src/main/java/com/hebo/springcloudapi/SpringcloudApiApplication.java
+++ springcloud-api/src/main/java/com/hebo/springcloudapi/SpringcloudApiApplication.java
@@ -3,6 +3,9 @@ package com.hebo.springcloudapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author HEBO
+ */
 @SpringBootApplication
 public class SpringcloudApiApplication {
 
